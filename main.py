ee19c384c6859f6a495c60325b93a48248ae4092
RymrtGnrs <105399509+RymrtGnrs@users.noreply.github.com>
2022-05-29 23:31:23 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -51,3 +51,39 @@ class MainWindow(QMainWindow):
 
         # Draw all the defined user interfaces
         self.uiCompLayout()
+    def uiCompLayout(self): 
+        self.counterUI()
+        self.startUI()
+        self.resetUI()
+        self.splitUI()
+        self.tableUI()
+
+    def showCounter(self):
+        if self.Start:
+            self.counter += 1
+            millisec = int((self.counter / 100 - int(self.counter /100)) * 100)
+
+            if millisec < 10:
+                self.millisec = '0' + str(millisec)
+            else:
+                self.millisec = str(millisec)
+
+            # Set the seconds value
+            if int(self.counter / 100) < 10:
+                self.seconds = '0' + str(int(self.counter / 100))
+            else:
+                self.seconds = str(int(self.counter / 100))
+
+                # Set the minutes value
+                if self.counter / 100 == 60.0:
+                    self.seconds = '00'
+                    self.counter = 0
+                    minutes = int(self.minutes) + 1
+                    if minutes < 10:
+                        self.minutes = '0' + str(minutes)
+                    else:
+                        self.minutes = str(minutes)
+
+        # Merge the minute, second, and millisec values
+        text = f"{self.minutes}m : {self.seconds}s : {self.millisec}"
+        self.counterlabel.setText('<h2>' + text + '</h2>')
