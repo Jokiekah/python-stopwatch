da6cbc8ea10612523824f8a274b6360ce12919f4
Jokiekah <105437577+Jokiekah@users.noreply.github.com>
2022-05-29 21:15:40 +0800

    Add files via upload
    code base

---
+++ main.py
@@ -0,0 +1,17 @@
+# Import all necessary packages and classes
+import sys
+
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QLabel,
+    QFrame,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QTableWidget,
+    QTableWidgetItem,
+    QHeaderView,
+)
+from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtGui import QFont
